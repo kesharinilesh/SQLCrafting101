b4777b5ab21c732eb74561258ea1b52fd45032af
Nilesh Keshari <kesharinilesh13@gmail.com>
2024-03-02 17:50:29 +0530

    solution

--- Students and Examinations.sql
+++ Students and Examinations.sql
@@ -4,7 +4,7 @@ SELECT
     Subjects.subject_name,
     COUNT(Examinations.subject_name) AS attended_exams
 FROM Students
-JOIN Subjects
+CROSS JOIN Subjects
 LEFT JOIN Examinations
 ON Students.student_id = Examinations.student_id
 AND Subjects.subject_name = Examinations.subject_name
